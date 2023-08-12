4da4ab7b97a41904ff2c80c2c00e03bc960d4a8d
sai sinare <saisinare19@gmail.com>
2023-08-12 11:05:56 +0530

    Update Offer Banner Of Home Page

--- src/components/home/OfferSlider.jsx
+++ src/components/home/OfferSlider.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const OfferSlider = () => {
   return (
 <>
-<div className="w-full mt-5 h-72 rounded-xl my-3 overflow-hidden bg-no-repeat bg-contain bg-center " >
-  <img src="img/home/offer.png" alt="" />
+<div className="w-full mt-5 h-72 rounded-xl my-3 overflow-hidden bg flex items-center justify-center hover:shadow-md transition-all duration-500 ease-in-out hover:shadow-emerald-700" style={{backgroundColor:"#13171a"}} >
+  <img src="img/home/offer.jpg"  alt="" />
   
 </div>
 </>
