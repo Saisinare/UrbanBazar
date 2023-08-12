e1ad2bbb20fff311b75c724c9569005ad6454176
sai sinare <saisinare19@gmail.com>
2023-08-12 11:11:17 +0530

    Update Offer Banner Component Add Link To Offer Page in Offer Banner Update Component Name To The Offerbanner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Categories from './Categories';
-import OfferSlider from './OfferSlider';
 import BestOfSection from './BestOfSection';
 import Footer from './Footer';
+import OfferBanner from './OfferSlider';
 const Home = () => {
-    
     return (
         <div className='px-5'>
         <Categories/>
-        <OfferSlider/>
+        <OfferBanner/>
         <BestOfSection/>
         <Footer/>
         </div>
--- src/components/home/OfferSlider.jsx
+++ src/components/home/OfferSlider.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const OfferSlider = () => {
+const OfferBanner = () => {
   return (
 <>
+<Link to="/offers">
 <div className="w-full mt-5 h-72 rounded-xl my-3 overflow-hidden bg flex items-center justify-center hover:shadow-md transition-all duration-500 ease-in-out hover:shadow-emerald-700" style={{backgroundColor:"#13171a"}} >
   <img src="img/home/offer.jpg"  alt="" />
   
 </div>
+</Link>
 </>
   );
 };
 
-export default OfferSlider;
+export default OfferBanner;
