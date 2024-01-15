06ac4db7123d152346de1289753d022ffe937411
sai sinare <saisinare19@gmail.com>
2024-01-15 12:24:52 +0530

    prinring the token

--- backend/middleware/isAuh.js
+++ backend/middleware/isAuh.js
@@ -6,7 +6,7 @@ const fetchuser = (req,res,next)=>{
     const token = req.cookies.token
     console.log(req.cookies.token)
     if(!token){
-        return res.status(401).json({message:"enter Valid Token"})
+        return res.status(401).json({message:"enter Valid Token",JWT:JWT_SECRETE,token:token})
     }
     const data = jwt.verify(token,JWT_SECRETE)
     req.userId = data.id 
