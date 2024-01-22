fb3e7c6db6385c593b91e95a21d8847ce0935ed5
sai sinare <saisinare19@gmail.com>
2024-01-22 10:32:05 +0530

    category responsiveness

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -19,10 +19,10 @@ const Category = (props) => {
         state={{ category: props.title.toLowerCase() }}
       >
         <div
-          class=" transition-all duration-500 ease-in-out bg-transparent backgroud bg-top cursor-pointer bg-no-repeat md:bg-cover sm:bg-contain bg-none hover:bg-left flex sm:h-10 md:h-60   hover:scale-105  p-3 rounded-xl bg-gray-800 mx-2 text-white items-start justify-center "
+          class=" transition-all duration-500 ease-in-out bg-transparent backgroud bg-top cursor-pointer bg-no-repeat md:bg-cover sm:bg-contain bg-none hover:bg-left flex sm:h-10 md:h-60   hover:scale-105  p-3 rounded-xl bg-gray-800 md:mx-2 text-white items-start justify-center "
           style={{ backgroundImage: props.imgurl }}
         >
-          <h1 className="text-black font-sans  font-semibold text-xl ">
+          <h1 className="text-black font-sans  font-semibold text-base md:text-xl  ">
             {props.title}
           </h1>
         </div>
