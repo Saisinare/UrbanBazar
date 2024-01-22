ae4f879ce6da79024c791348b2675a9c4bf2a4e0
sai sinare <saisinare19@gmail.com>
2024-01-22 10:04:48 +0530

    bug fixing

--- src/components/cart/Heading.jsx
+++ src/components/cart/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div className="h-20 w-full flex items-center justify-start px-5 ">
+    <div className="h-20 w-full flex items-center justify-start px-5 md:pl-14 ">
       <h1 className=" font-bold text-2xl text-black">My Cart</h1>
     </div>
   );
