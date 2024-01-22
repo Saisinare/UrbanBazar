5d392cd6ad524079570d3d83b29efff90fe65ad8
sai sinare <saisinare19@gmail.com>
2024-01-22 09:42:20 +0530

    offer image path update

--- src/components/home/OfferSlider.jsx
+++ src/components/home/OfferSlider.jsx
@@ -5,8 +5,7 @@ const OfferBanner = () => {
 <>
 <Link to="/offers">
 <div className="w-full mt-5 h-72 rounded-xl my-3 overflow-hidden bg flex items-center justify-center hover:shadow-md transition-all duration-500 ease-in-out hover:shadow-emerald-700" style={{backgroundColor:"#13171a"}} >
-  <img src="img/home/offer.jpg"  alt="" />
-  
+  <img src={`${process.env.REACT_APP_BACKEND_API_URL}/icons/offer.JPG`}  alt="" />
 </div>
 </Link>
 </>
