e07e701d31f78c13880764e5d036bd9672c442d2
sai sinare <saisinare19@gmail.com>
2024-01-22 20:32:06 +0530

    filter responsiveness stage 1

--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -26,37 +26,37 @@ const handleFilter =()=>{
   return (
 
     <>
-      <div className=" h-96 w-7/12 mt-3 p-3 bg-gray-50 backdrop-blur-lg rounded-xl absolute translate-x-72 flex flex-col">
-        <div className="w-full h-24  flex flex-col">
+      <div className="h-fit md:h-96 w-11/12 md:w-7/12  mt-3 p-3 bg-gray-50 backdrop-blur-lg rounded-xl absolute md:translate-x-72 flex flex-col ">
+        <div className="w-full h-fit  flex flex-col">
           <div className="w-full h-2/5  px-2 flex  items-center font-bold ">
             Categories
           </div>
-          <div className="w-full h-3/5 flex p-1 flex-wrap gap-1">
+          <div className="w-full h-3/5 flex p-1 flex-wrap md:flex-row gap-1">
             {categories.map((category) => {
               return (
-                <div className={`${productsState.filters.category == category.title && 'border border-green-400'} w-36 h-full bg-slate-200 rounded-md flex items-center justify-center font-semibold cursor-pointer `} onClick={handlecatFilters}>
+                <div className={`${productsState.filters.category == category.title && 'border border-green-400'} w-36 h-12   bg-slate-200 rounded-md flex items-center justify-center font-semibold cursor-pointer `} onClick={handlecatFilters}>
                   {category.title}
                 </div>
               );
             })}
           </div>
         </div>
-        <div className="w-full h-24  flex flex-col">
+        <div className="w-full h-fit md:h-24  flex flex-col">
           <div className="w-full h-2/5  px-2 flex  items-center font-bold ">
             Minimum Price ₹
           </div>
-          <div className="w-full h-3/5 flex p-1 flex-wrap gap-1 justify-around">
-            <input type="text" className=" h-10 w-1/6 border-slate-300 rounded-md font-semibold input-fields" id='minPrice' />
-            <input type="range" className="w-4/5" onChange={handleRange} />
+          <div className="w-full h-3/5 flex p-1 md:flex-wrap gap-1 justify-around md:flex-row flex-col">
+            <input type="text" className=" h-10 w-3/6 border-slate-300 rounded-md font-semibold input-fields" id='minPrice' />
+            <input type="range" className="w-full md:w-4/5" onChange={handleRange} />
           </div>
         </div>
         <div className="w-full h-24  flex flex-col">
           <div className="w-full h-2/5  px-2 flex  items-center font-bold ">
             Maximum Price ₹
           </div>
-          <div className="w-full h-3/5 flex p-1 flex-wrap gap-1 justify-around">
-            <input type="text" className=" h-10 w-1/6 border-slate-300 rounded-md font-semibold input-fields" id="maxPrice"/>
-            <input type="range" className="w-4/5" onChange={handleRange} />
+          <div className="w-full h-3/5 flex p-1 md:flex-wrap gap-1 justify-around md:flex-row flex-col ">
+            <input type="text" className=" h-10 w-3/6 border-slate-300 rounded-md font-semibold input-fields" id="maxPrice"/>
+            <input type="range" className=" w-full md:w-4/5" onChange={handleRange} />
           </div>
         </div>
         <div className="flex items-center justify-end">
