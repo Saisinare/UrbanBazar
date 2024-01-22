5da22e2159febafc9edc4c2bbbeeee4ecd054a5e
sai sinare <saisinare19@gmail.com>
2024-01-22 17:00:21 +0530

    navbar responsiveness completed

---
+++ src/components/HambergerMenu.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setLogin, setSellerMode } from "../redux/slice/user";
+import { setlogin } from "../redux/slice/login";
+import Cookies from "js-cookie";
+
+const HambergerMenu = (props) => {
+  const userstate = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSwitch = (e) => {
+    if (e.target.checked) {
+      dispatch(setSellerMode(true));
+      navigate("/seller/products");
+    } else {
+      dispatch(setSellerMode(false));
+      navigate("/");
+    }
+  };
+
+  const handleLogout = () => {
+    Cookies.remove("login");
+    Cookies.remove("token");
+    dispatch(setLogin(false));
+    dispatch(setlogin(false));
+  };
+
+  return (
+    <div
+      className={` bg-white px-8 h-screen w-screen fixed z-50   md:hidden transition-all duration-200 ease-linear ${
+        props.menu ? " ml-0 opacity-100" : "-ml-96 opacity-0 "
+      } `}
+    >
+      <div className=" w-full h-20 border-b flex justify-between items-center ">
+        <div className=" w-fit">Seller Mode</div>
+        <div>
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              class="sr-only peer"
+              id="seller-switch "
+              onChange={handleSwitch}
+              checked={userstate.SellerMode}
+            />
+            <div class="w-8 h-4 rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[5px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all bg-slate-200 hover:bg-slate-300 transition-all ease-in duration-100 peer-checked:bg-green-600"></div>
+          </label>
+        </div>
+      </div>
+
+      <Link to={`${userstate.SellerMode ? "seller/dashboard" : "/shop"}`}>
+        <div className=" w-full h-20 border-b flex justify-start items-center">
+          <div className=" w-1/12">
+            <i className=" fa  fa-shopping-bag"></i>
+          </div>
+          <div>Shop</div>
+        </div>
+      </Link>
+
+      <Link to={`${userstate.SellerMode ? "seller/products" : "/orders"}`}>
+        <div className=" w-full h-20 border-b flex justify-start items-center">
+          <div className=" w-1/12">
+            <i class="fa-solid fa-box"></i>
+          </div>
+          <div>My Orders</div>
+        </div>
+      </Link>
+
+      {userstate.SellerMode && (
+        <Link to={`/seller/addproduct`}>
+          <div className=" w-full h-20 border-b flex justify-start items-center">
+            <div className=" w-1/12">
+              <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>Add Product</div>
+          </div>
+        </Link>
+      )}
+      <Link to={"/profile"}>
+        <div className=" w-full h-20 border-b flex justify-start items-center">
+          <div className=" w-1/12">
+            <i class="fa-solid fa-user"></i>
+          </div>
+          <div>My Profile</div>
+        </div>
+      </Link>
+
+      <div className=" w-full h-20 border-b flex justify-start items-center">
+        <div className=" w-1/12" onClick={handleLogout}>
+          <i class="fa-solid fa-right-from-bracket"></i>
+        </div>
+        <div>Logout</div>
+      </div>
+    </div>
+  );
+};
+
+export default HambergerMenu;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,6 +10,7 @@ import Filter from "./Filters/Filter";
 import MyLoadingBar from "./MyLoadingBar";
 import { setisComplete, setisWaiting } from "../redux/slice/progressBar";
 import jsCookie from "js-cookie";
+import HambergerMenu from "./HambergerMenu";
 
 const NavBar = () => {
   const [progress, setprogress] = useState(0);
@@ -22,7 +23,7 @@ const NavBar = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const [profileCard, setprofileCard] = useState(false);
-
+  const [menu, setmenu] = useState(false)
   const disptach = useDispatch();
 
   useEffect(() => {
@@ -227,6 +228,7 @@ const NavBar = () => {
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
               aria-controls="navbar-search"
               aria-expanded="false"
+              onClick={ ()=>{setmenu(!menu)}}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -246,41 +248,11 @@ const NavBar = () => {
               </svg>
             </button>
           </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-search"
-          >
-            <div className="relative mt-3 md:hidden ">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-              />
-            </div>
-          </div>
         </div>
         {location.pathname.includes("/shop") && FilterVisible && <Filter />}
       </nav>
       <MyLoadingBar progress={progress} />
+       <HambergerMenu menu={menu}/>
     </>
   );
 };
